335d695659f8ff40c184809f0688dabbbd3e19a9
Antonio Matheus <antonimathsilva@gmail.com>
2025-09-22 08:00:27 -0300

    Add HTMLNode class

---
+++ src/test_htmlnode.py
@@ -0,0 +1,14 @@
+import unittest
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        html_node = HTMLNode(
+            props={"href": "https://www.example.com", "style": "color=red"}
+        )
+        html_attrs = html_node.props_to_html()
+        self.assertEqual(
+            html_attrs, ' href="https://www.example.com" style="color=red"'
+        )
+
