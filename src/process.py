feb57fdc38e29c88abbff3234c912d53e2634fe2
Antonio Matheus <antonimathsilva@gmail.com>
2025-09-28 10:05:45 -0300

    Extract image and link from markdown

--- src/process.py
+++ src/process.py
@@ -1,3 +1,4 @@
+import re
 from src.textnode import TextNode
 
 def split_textnodes_delimiter(old_nodes, delimiter, text_type):
@@ -7,10 +8,17 @@ def split_textnodes_delimiter(old_nodes, delimiter, text_type):
         for idx, text_split in enumerate(text.split(delimiter)):
             if len(text_split) == 0:
                 continue
-            
+
             if idx % 2 == 0:
                 new_nodes.append(TextNode(text_split, node.text_type))
             else:
                 new_nodes.append(TextNode(text_split, text_type))
     return new_nodes
         
+def extract_markdown_images(text):
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    return re.findall(pattern, text)
+
+def extract_markdown_links(text):
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    return re.findall(pattern, text)
--- tests/test_process.py
+++ tests/test_process.py
@@ -2,7 +2,8 @@ import unittest
 from src.process import *
 from src.textnode import *
 
-class TestProcess(unittest.TestCase):
+
+class TestSplitTextNodes(unittest.TestCase):
     def test_apostrophe(self):
         node = TextNode("This is a text with a `code block` word", TextType.PLAIN)
         new_nodes = split_textnodes_delimiter([node], "`", TextType.CODE)
@@ -16,13 +17,20 @@ class TestProcess(unittest.TestCase):
     def test_multiple_nodes(self):
         node1 = TextNode("This is a text with a `code block` word", TextType.PLAIN)
         node2 = TextNode("This is a text with a `second code block`", TextType.PLAIN)
-        new_nodes = split_textnodes_delimiter([node1, node2,], "`", TextType.CODE)
+        new_nodes = split_textnodes_delimiter(
+            [
+                node1,
+                node2,
+            ],
+            "`",
+            TextType.CODE,
+        )
         expected_output = [
             TextNode("This is a text with a ", TextType.PLAIN),
             TextNode("code block", TextType.CODE),
             TextNode(" word", TextType.PLAIN),
             TextNode("This is a text with a ", TextType.PLAIN),
-            TextNode("second code block", TextType.CODE)
+            TextNode("second code block", TextType.CODE),
         ]
         self.assertEqual(new_nodes, expected_output)
 
@@ -35,3 +43,17 @@ class TestProcess(unittest.TestCase):
             TextNode(" word", TextType.PLAIN),
         ]
         self.assertEqual(new_nodes, expected_output)
+
+
+class TestMarkdownExtraction(unittest.TestCase):
+    def test_extract_markdown_image(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is a text with an [Click me!](https://www.example.com)"
+        )
+        self.assertListEqual([("Click me!", "https://www.example.com")], matches)
